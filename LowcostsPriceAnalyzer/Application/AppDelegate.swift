af13f007a6ac9950d451f6355d7896d21d712da4
Maksim Rakhleev <mrakhleev@tango.me>
2023-01-12 23:18:17 +0100

    Remove scene.

---
+++ LowcostsPriceAnalyzer/Application/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  LowcostsPriceAnalyzer
+//
+//  Created by Maksim Rakhleev on 12.01.23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+}
---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '12.0'
+
+target 'LowcostsPriceAnalyzer' do
+  use_frameworks!
+end
