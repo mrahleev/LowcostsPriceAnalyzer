33f12dbc8e6cf96f6e91d006637b63c4d4eb50f6
Maksim Rakhleev <mrakhleev@tango.me>
2023-01-12 22:34:04 +0100

    Project init

---
+++ LowcostsPriceAnalyzer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LowcostsPriceAnalyzer
+//
+//  Created by Maksim Rakhleev on 12.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
